c2213d17053b4d65d1a48ab2e5523d588c7f7f10
gustaaragao <gustavohenriquearagaosilva@gmail.com>
2025-08-19 09:22:39 -0300

    async

--- projeto-api/tests/test_db.py
+++ projeto-api/tests/test_db.py
@@ -1,19 +1,21 @@
 from dataclasses import asdict
 
+import pytest
 from sqlalchemy import select
 
 from projeto_api.models import User
 
 
-def test_create_user(session, mock_db_time):
+@pytest.mark.asyncio
+async def test_create_user(session, mock_db_time):
     with mock_db_time(model=User) as time:
         new_user = User(
             username='test', password='secret', email='test@email.com'
         )
         session.add(new_user)
-        session.commit()
+        await session.commit()
 
-    user = session.scalar(select(User).where(User.username == 'test'))
+    user = await session.scalar(select(User).where(User.username == 'test'))
 
     assert asdict(user) == {
         'id': 1,
